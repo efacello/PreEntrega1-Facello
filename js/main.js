34668d08f68c101c28fc6c7b3d5c1f2356a1e838
efacello <123125625+efacello@users.noreply.github.com>
2023-03-12 20:27:37 -0300

    Update main.js
    mejorando la cuestion

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+document.querySelector('#calcular').addEventListener('click', () =>{
+const n1 = parseInt(document.querySelector('#n1').value);
+const n2 = parseInt(document.querySelector('#n2').value);
+const op = document.querySelector('#op').value;
+let r;
 
+if (op == '+'){
+  r = n1 + n2;
+} else if (op == '-'){
+  r = n1 - n2;
+} else if (op == '*'){
+  r = n1 * n2;
+} else if (op == '/'){
+  r = n1 / n2;
+}
+console.log(r)
+});
